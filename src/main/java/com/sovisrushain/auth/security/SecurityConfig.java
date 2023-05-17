cb62551eddd0e4b7f7c9f924551041582f21174c
sovisrushain <sovisrushain@gmail.com>
2023-05-17 16:30:54 +0530

    config security filter chain

--- src/main/java/com/sovisrushain/auth/security/SecurityConfig.java
+++ src/main/java/com/sovisrushain/auth/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sovisrushain.auth.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,10 @@ public class SecurityConfig {
         return http
                 .csrf()
                 .disable()
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.GET, "/heroes/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
                 .httpBasic()
                 .and()
                 .build();
