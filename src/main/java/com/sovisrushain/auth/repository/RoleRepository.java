f65e6b7bd3127cd66acb11307e260eeb15fc2576
sovisrushain <sovisrushain@gmail.com>
2023-05-17 17:27:45 +0530

    user and role repository

---
+++ src/main/java/com/sovisrushain/auth/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.sovisrushain.auth.repository;
+
+import com.sovisrushain.auth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
