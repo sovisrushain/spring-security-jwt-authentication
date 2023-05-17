3d3e8c691c6ea9ad5d1c781a8d269701b4f38bcf
sovisrushain <sovisrushain@gmail.com>
2023-05-17 17:13:50 +0530

    users and roles

---
+++ src/main/java/com/sovisrushain/auth/model/Role.java
@@ -0,0 +1,18 @@
+package com.sovisrushain.auth.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
