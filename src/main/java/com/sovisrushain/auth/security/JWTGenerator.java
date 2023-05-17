5d70c2aec9abc6efff3500cd16ecd443a6e74d6c
sovisrushain <sovisrushain@gmail.com>
2023-05-17 23:22:25 +0530

    fix bugs

--- src/main/java/com/sovisrushain/auth/security/JWTGenerator.java
+++ src/main/java/com/sovisrushain/auth/security/JWTGenerator.java
@@ -1,6 +1,5 @@
 package com.sovisrushain.auth.security;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
@@ -16,21 +15,20 @@ public class JWTGenerator {
         Date currentDate = new Date();
         Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
 
-        String token = Jwts.builder()
+        return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expireDate)
                 .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
                 .compact();
-        return token;
     }
 
     public String getUsernameFromJWT(String token) {
-        Claims claims = Jwts.parser()
+        return Jwts.parser()
                 .setSigningKey(SecurityConstants.JWT_SECRET)
                 .parseClaimsJws(token)
-                .getBody();
-        return claims.getSubject();
+                .getBody()
+                .getSubject();
     }
 
     public boolean validateToken(String token) {
